e6c6955e79a2d65e2d3a7ef2af0d766e730f0620
Matthias Lagrabe <matthias.lagrabe@gmail.com>
2022-01-10 15:28:13 +0100

    added main logic

---
+++ js/app.js
@@ -0,0 +1,81 @@
+const eleve = [
+  [
+    "Jonathan DEMAY",
+    "https://neuillycdg.espace.link/documents/613f832d5a72d/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+  ],
+  [
+    "Aghilas NAIT AMER",
+    "https://neuillycdg.espace.link/documents/614208cebd9db/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+  ],
+  [
+    "Océane RAUBITSCHEK",
+    "https://neuillycdg.espace.link/documents/613a23274a247/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+  ],
+  [
+    "Mendy STÉBOU",
+    "https://neuillycdg.espace.link/documents/613ad4388c482/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+  ],
+  [
+    "Johnny CHEN",
+    "https://neuillycdg.espace.link/documents/61379796099e9/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+  ],
+  [
+    "Afaf LABED",
+    "https://neuillycdg.espace.link/documents/6139187edc7a4/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+  ],
+  [
+    "Alois DELAUNE",
+    "https://neuillycdg.espace.link/documents/613d3aff451ee/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+  ],
+  [
+    "Selim GOMRI",
+    "https://neuillycdg.espace.link/documents/613c58fcbdc8b/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+  ],
+  [
+    "Simon COSCINO-MOLLARD ",
+    "https://neuillycdg.espace.link/documents/613b83e746682/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+  ],
+  [
+    "Nelson BELGARDE",
+    "https://neuillycdg.espace.link/documents/613a1626c441c/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+  ],
+  [
+    "Nathalie LAVROFF",
+    "https://neuillycdg.espace.link/documents/6139d35078296/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+  ],
+  [
+    "Saidi MBELO",
+    "https://neuillycdg.espace.link/documents/613adc6a73c97/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+  ],
+  [
+    "Nicolas CHALONO",
+    "https://neuillycdg.espace.link/documents/6137c2878826d/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+  ],
+  [
+    "Nicolas SCHWACHTGEN",
+    "https://neuillycdg.espace.link/documents/613a6642ef49a/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+  ],
+  [
+    "Matthias LAGRABE",
+    "https://neuillycdg.espace.link/documents/614214e0c0e65/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+  ],
+  [
+    "Kalvin BELONY",
+    "https://neuillycdg.espace.link/documents/6142f83fa401a/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+  ],
+];
+
+function generateEleve() {
+  //select div to append a random eleve with his picture
+  const div = document.querySelector(".eleve");
+  const name = document.querySelector(".eleve-name");
+
+  //generate a random number between 0 and the length of the array
+  const random = Math.floor(Math.random() * eleve.length);
+
+  //append the random eleve with his picture
+  div.style.backgroundImage = `url(${eleve[random][1]})`;
+  div.style.backgroundSize = "cover";
+  div.style.backgroundPosition = "center";
+  name.innerHTML = eleve[random][0];
+}
