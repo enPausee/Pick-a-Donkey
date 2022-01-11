736d31ea29481a086d73339827b8a24cde695e9f
Matthias Lagrabe <matthias.lagrabe@gmail.com>
2022-01-11 13:04:51 +0100

    improved image quality

--- js/app.js
+++ js/app.js
@@ -1,73 +1,77 @@
 const eleve = [
   [
     "Jonathan DEMAY",
-    "https://neuillycdg.espace.link/documents/613f832d5a72d/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+    "https://neuillycdg.espace.link/documents/613f832d5a72d/thumbnail/600x600?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
   ],
   [
     "Aghilas NAIT AMER",
-    "https://neuillycdg.espace.link/documents/614208cebd9db/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+    "https://neuillycdg.espace.link/documents/614208cebd9db/thumbnail/600x600?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
   ],
   [
     "Océane RAUBITSCHEK",
-    "https://neuillycdg.espace.link/documents/613a23274a247/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+    "https://neuillycdg.espace.link/documents/613a23274a247/thumbnail/600x600?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
   ],
   [
     "Mendy STÉBOU",
-    "https://neuillycdg.espace.link/documents/613ad4388c482/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+    "https://neuillycdg.espace.link/documents/613ad4388c482/thumbnail/600x600?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
   ],
   [
     "Johnny CHEN",
-    "https://neuillycdg.espace.link/documents/61379796099e9/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+    "https://neuillycdg.espace.link/documents/61379796099e9/thumbnail/600x600?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
   ],
   [
     "Afaf LABED",
-    "https://neuillycdg.espace.link/documents/6139187edc7a4/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+    "https://neuillycdg.espace.link/documents/6139187edc7a4/thumbnail/600x600?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
   ],
   [
     "Alois DELAUNE",
-    "https://neuillycdg.espace.link/documents/613d3aff451ee/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+    "https://neuillycdg.espace.link/documents/613d3aff451ee/thumbnail/600x600?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
   ],
   [
     "Selim GOMRI",
-    "https://neuillycdg.espace.link/documents/613c58fcbdc8b/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+    "https://neuillycdg.espace.link/documents/613c58fcbdc8b/thumbnail/600x600?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
   ],
   [
     "Simon COSCINO-MOLLARD ",
-    "https://neuillycdg.espace.link/documents/613b83e746682/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+    "https://neuillycdg.espace.link/documents/613b83e746682/thumbnail/600x600?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
   ],
   [
     "Nelson BELGARDE",
-    "https://neuillycdg.espace.link/documents/613a1626c441c/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+    "https://neuillycdg.espace.link/documents/613a1626c441c/thumbnail/600x600?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
   ],
   [
     "Nathalie LAVROFF",
-    "https://neuillycdg.espace.link/documents/6139d35078296/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+    "https://neuillycdg.espace.link/documents/6139d35078296/thumbnail/600x600?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
   ],
   [
     "Saidi MBELO",
-    "https://neuillycdg.espace.link/documents/613adc6a73c97/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+    "https://neuillycdg.espace.link/documents/613adc6a73c97/thumbnail/600x600?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
   ],
   [
     "Nicolas CHALONO",
-    "https://neuillycdg.espace.link/documents/6137c2878826d/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+    "https://neuillycdg.espace.link/documents/6137c2878826d/thumbnail/600x600?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
   ],
   [
     "Nicolas SCHWACHTGEN",
-    "https://neuillycdg.espace.link/documents/613a6642ef49a/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+    "https://neuillycdg.espace.link/documents/613a6642ef49a/thumbnail/600x600?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
   ],
   [
     "Matthias LAGRABE",
-    "https://neuillycdg.espace.link/documents/614214e0c0e65/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+    "https://neuillycdg.espace.link/documents/614214e0c0e65/thumbnail/600x600?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
   ],
   [
     "Kalvin BELONY",
-    "https://neuillycdg.espace.link/documents/6142f83fa401a/thumbnail/70x70?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
+    "https://neuillycdg.espace.link/documents/6142f83fa401a/thumbnail/600x600?access_token=OGU3NzRiNjIxZTM2N2JlYTA0OWY2MWZhZjk4YTVlZDRmYzI1NDhhYjcyNjlhNWQ4NGVlMjk4MTY5YWZkNjU0OQ",
   ],
 ];
 
 const alreadyPicked = [];
 const listAlreadyPicked = document.querySelector(".alreadyPicked");
 const btnGenerate = document.querySelector("button");
+const btnBefore = document.querySelector(".btn-before");
+const btnNext = document.querySelector(".btn-next");
+
+let currentEleveIndex = 0;
 
 function generateEleve() {
   //check if there is still available eleve
@@ -92,13 +96,13 @@ function generateEleve() {
     div.style.backgroundPosition = "center";
     updateName(eleve[random][0]);
     alreadyPicked.push(eleve[random][0]);
+    currentEleveIndex = alreadyPicked.length - 1;
   }
 }
 
 function updateName(eleve) {
   //get dom elements name
   const name = document.querySelector(".eleve-name");
-
   name.innerHTML = eleve;
 }
 
@@ -123,7 +127,6 @@ function appendAlreadyPicked() {
   
   btn.addEventListener("click", () => {
     const div = document.querySelector(".eleve");
-    const name = document.querySelector(".eleve-name");
     div.style.backgroundImage = `url(${eleve[index][1]})`;
     updateName(eleve[index][0]);
   });
@@ -153,3 +156,27 @@ btnGenerate.addEventListener("click", () => {
     updateCounter();
   }
 });
+
+//add an event listener to the before button that will update the name and picture of the eleve to the previous one in alreadyPicked array
+btnBefore.addEventListener("click", () => {
+  if (currentEleveIndex === 0) {
+    return null;
+  } else {
+    const div = document.querySelector(".eleve");
+    div.style.backgroundImage = `url(${eleve[currentEleveIndex - 1][1]})`;
+    updateName(eleve[currentEleveIndex - 1][0]);
+  }
+});
+
+//add an event listener to the next button that will update the name and picture of the eleve to the next one in alreadyPicked array
+btnNext.addEventListener("click", () => {
+  if (currentEleveIndex === eleve.length - 1) {
+    return null;
+  } else {
+    const div = document.querySelector(".eleve");
+    div.style.backgroundImage = `url(${eleve[currentEleveIndex + 1][1]})`;
+    updateName(eleve[currentEleveIndex + 1][0]);
+  }
+});
+
+
